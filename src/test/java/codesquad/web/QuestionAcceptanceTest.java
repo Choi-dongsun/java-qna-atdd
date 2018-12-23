77b742019e5d9ce083031ee695a5b430bf798f6a
Choi-dongsun <rovks91@gmail.com>
2018-12-23 17:38:04 +0900

    Question 생성 기능구현 by ATDD

---
+++ src/test/java/codesquad/web/QuestionAcceptanceTest.java
@@ -0,0 +1,50 @@
+package codesquad.web;
+
+import codesquad.domain.QuestionRepository;
+import codesquad.domain.User;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import support.test.AcceptanceTest;
+import support.test.HtmlFormDataBuilder;
+
+public class QuestionAcceptanceTest extends AcceptanceTest {
+    private static final Logger log = LoggerFactory.getLogger(QuestionAcceptanceTest.class);
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    public void createFormWithoutLogin() throws Exception {
+        ResponseEntity<String> response = template().getForEntity("/questions/form", String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+        log.debug("body : {}", response.getBody());
+    }
+
+    @Test
+    public void createForm() throws Exception {
+        ResponseEntity<String> response = basicAuthTemplate().getForEntity("/questions/form", String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        log.debug("body : {}", response.getBody());
+    }
+
+    @Test
+    public void create() throws Exception {
+        HttpEntity<MultiValueMap<String, Object>> request =
+                HtmlFormDataBuilder.urlEncodedForm()
+                        .addParameter("title", "질문제목")
+                        .addParameter("contents", "질문내용")
+                        .build();
+
+        ResponseEntity<String> response = basicAuthTemplate().postForEntity("/questions", request, String.class);
+
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
+        softly.assertThat(questionRepository.findByTitle("질문제목").isPresent()).isTrue();
+        softly.assertThat(response.getHeaders().getLocation().getPath()).startsWith("/");
+    }
+}
