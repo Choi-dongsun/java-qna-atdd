56a6f137ec731d01b5bb43381d0c42bcc2837788
moving-line <movinglinecheck@gmail.com>
2019-05-18 00:17:25 +0900

    Add Question createForm func
    - 질문하기 페이지 접속 기능 구현

---
+++ src/test/java/codesquad/web/QuestionAcceptanceTest.java
@@ -0,0 +1,31 @@
+package codesquad.web;
+
+import codesquad.domain.QuestionRepository;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import support.test.AcceptanceTest;
+
+public class QuestionAcceptanceTest extends AcceptanceTest {
+    private static final Logger log = LoggerFactory.getLogger(QuestionAcceptanceTest.class);
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    public void createForm() throws Exception {
+        ResponseEntity<String> response = basicAuthTemplate().getForEntity("/questions/form", String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        log.debug("body : {}", response.getBody());
+    }
+
+    @Test
+    public void createForm_no_login() throws Exception {
+        ResponseEntity<String> response = template().getForEntity("/questions/form", String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+        log.debug("body : {}", response.getBody());
+    }
+}
